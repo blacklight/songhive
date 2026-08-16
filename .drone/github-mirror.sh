749522004291c26f946d70504f6bac215550bf06
Fabio Manganiello <fabio@manganiello.tech>
2026-08-17 00:13:53 +0200

    ci: Add Drone pipeline to mirror repo to GitHub
    - Add .drone.yml and mirror step using SSH secrets
    - Add scripts to configure git/ssh and push branches/tags to GitHub

---
+++ .drone/github-mirror.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. .drone/macros/configure-git.sh
+. .drone/macros/configure-ssh.sh
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+
+# Clone the repository
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [ -z "${branch}" ]; then
+  echo "No branch checked out"
+  exit 1
+fi
+
+git remote add github git@github.com:/blacklight/songhive.git
+
+if [[ "$branch" == "main" ]]; then
+  git pull --rebase github "${branch}" || echo "No such branch on Github"
+fi
+
+# Push the changes to the GitHub mirror
+git push -f --all -v github
+git push --tags -v github
